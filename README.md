6a0c5f2fec88938e72a2df1f44fd84e37300d965
Eduardo Yáñez Parareda <eduardo.yanez@gmail.com>
2012-05-07 18:56:43 +0200

    * Hero can fire arrows * Hero has 3 lives * Hero dies when touchs a monster * When hero loses 3 lives the game is over * Added some instructions to README file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # A simple HTML5 + KineticJS game
 
+##Instructions
+
+You get points killing monsters with your arrows. If you touch a monster you die, and when you lose 3 lives the game is over. 
+
+##Controls
+
+Move: Cursor keys
+Fire: Ctrl
--- js/game.js
+++ js/game.js
@@ -1,183 +1,284 @@
-// Create the main surface
-var stage = new Kinetic.Stage({
-	container: "surface",
-	width: 512,
-	height: 480
-});
-
-var layer = new Kinetic.Layer();
-
-// Background image
-var bgReady = false;
-var bgImageObj = new Image();
-bgImageObj.onload = function () {
-	bgReady = true;
-};
-bgImageObj.src = "images/background.png";
-var bgImage = new Kinetic.Image({
-	x:0,
-	y:0,
-	image:bgImageObj
-});
-
-// Hero image
-var heroReady = false;
-var heroImageObj = new Image();
-heroImageObj.onload = function () {
-	heroReady = true;
-};
-heroImageObj.src = "images/hero.png";
-var heroImage = new Kinetic.Image({
-	image:heroImageObj
-});
-
-// Monster image
-var monsterReady = false;
-var monsterImageObj = new Image();
-monsterImageObj.onload = function () {
-	monsterReady = true;
-};
-monsterImageObj.src = "images/monster.png";
-var monsterImage = new Kinetic.Image({
-	image:monsterImageObj
-});
-
-// score
-var score = new Kinetic.Text({
-	text: "Goblins caught: 0",
-	x: 32,
-	y: 32,
-	align: "left",
-	verticalAlign: "top",
-	fontFamily: "Helvetica",
-	fontSize: "24",
-	textFill: "white"
-});
-
-var then = 0;
-var now = 0;
-var delta = 0;
-
-// Game objects
-var hero = {
-	speed: 256 // movement in pixels per second
-};
-var monster = {};
-var monstersCaught = 0;
-
-
-// Handle keyboard controls
-var keysDown = {};
-
-addEventListener("keydown", function (e) {
-	keysDown[e.keyCode] = true;
-}, false);
-
-addEventListener("keyup", function (e) {
-	delete keysDown[e.keyCode];
-}, false);
-
-
-// Prevents that the screen scrolls when the user moves the finger
-var preventMove = function(e) {
-	   e.preventDefault();
-	   window.scroll(0, 0);
-	   return false;
-};
-//window.document.addEventListener('touchmove', preventMove, false);
+window.onload = function() {
+	// Create the main surface
+	var stage = new Kinetic.Stage({
+		container: "surface",
+		width: 512,
+		height: 540
+	});
 	
-var handleTouch = function (e) {
-	alert("Hola dedo");
-	var touches = e.touches;
-	for (i = 0; i < touches.length; i++) {
-		var touch = e.touches[i];
-		var x = touch.clientX;
-		var y = touch.clientY;
+	var layer = new Kinetic.Layer();
+	
+	// Background image
+	var bgReady = false;
+	var bgImageObj = new Image();
+	bgImageObj.onload = function () {
+		bgReady = true;
+	};
+	bgImageObj.src = "images/background.png";
+	var bgImage = new Kinetic.Image({
+		x:0,
+		y:60,
+		image:bgImageObj
+	});
+	
+	// Hero image
+	var heroReady = false;
+	var heroImageObj = new Image();
+	heroImageObj.onload = function () {
+		heroReady = true;
+	};
+	heroImageObj.src = "images/hero.png";
+	var heroImage = new Kinetic.Image({
+		image:heroImageObj
+	});
+	
+	// Monster image
+	var monsterReady = false;
+	var monsterImageObj = new Image();
+	monsterImageObj.onload = function () {
+		monsterReady = true;
+	};
+	monsterImageObj.src = "images/monster.png";
+	var monsterImage = new Kinetic.Image({
+		image:monsterImageObj
+	});
+	
+	// Arrow image
+	var arrowReady = false;
+	var arrowImageObj = new Image();
+	arrowImageObj.onload = function () {
+		arrowReady = true;
+	};
+	arrowImageObj.src = "images/arrow_right.png";
+	var arrowImage = new Kinetic.Image({
+		image:arrowImageObj,
+		visible:false
+	});
+	
+	// scoreText
+	var scoreText = new Kinetic.Text({
+		text: "Score: 0",
+		x: 125,
+		y: 32,
+		align: "left",
+		verticalAlign: "top",
+		fontFamily: "Helvetica",
+		fontSize: "20",
+		textFill: "black"
+	});
+	
+	// LivesText
+	var livesText = new Kinetic.Text({
+		text: "Lives: 3",
+		x: 0,
+		y: 32,
+		align: "left",
+		verticalAlign: "top",
+		fontFamily: "Helvetica",
+		fontSize: "20",
+		textFill: "black"
+	});
+	
+	// Game over
+	var gameOverText = new Kinetic.Text({
+		text: "GAME OVER",
+		align: "left",
+		verticalAlign: "top",
+		fontFamily: "Helvetica",
+		fontSize: "20",
+		textFill: "black"
+	});
+	
+	var then = 0;
+	var now = 0;
+	var delta = 0;
+	
+	// Game objects
+	var hero = {
+		// Movement in pixels per second
+		speed: 128 
+	};
+	var monster = {};
+	var monstersCaught = 0;
+	
+	var lives = 3;
+	var gameOver = false;
+	
+	// Fire
+	var arrow = {
+		speed: 256
+	};
+	
+	
+	// Handle keyboard controls
+	var keysDown = {};
+	
+	addEventListener("keydown", function (e) {
+		keysDown[e.keyCode] = true;
+	}, false);
+	
+	addEventListener("keyup", function (e) {
+		delete keysDown[e.keyCode];
+	}, false);
+	
+
+	/*
+	// Prevents that the screen scrolls when the user moves the finger
+	var preventMove = function(e) {
+		   e.preventDefault();
+		   window.scroll(0, 0);
+		   return false;
+	};
+	//window.document.addEventListener('touchmove', preventMove, false);
 		
-		hero.x = x;
-		hero.y = y;
+	var handleTouch = function (e) {
+		var touches = e.touches;
+		for (i = 0; i < touches.length; i++) {
+			var touch = e.touches[i];
+			var x = touch.clientX;
+			var y = touch.clientY;
+			
+			hero.x = x;
+			hero.y = y;
+		}
 	}
-}
-
-var handleMouse = function (e) {
-	alert("Hola mouse");
-	hero.x = e.clientX;
-	hero.y = e.clientY;
-}
-
-// Reset the game when the player catches a monster
-var reset = function () {
-	then = Date.now();
-	hero.x = stage.getWidth() / 2;
-	hero.y = stage.getHeight() / 2;
-
-	// Throw the monster somewhere on the screen randomly
-	monster.x = 32 + (Math.random() * (stage.getWidth() - 64));
-	monster.y = 32 + (Math.random() * (stage.getHeight() - 64));
-};
-
-// Update game objects
-var update = function (modifier) {
-	if (38 in keysDown) { // Player holding up
-		hero.y -= hero.speed * modifier;
-	}
-	if (40 in keysDown) { // Player holding down
-		hero.y += hero.speed * modifier;
-	}
-	if (37 in keysDown) { // Player holding left
-		hero.x -= hero.speed * modifier;
-	}
-	if (39 in keysDown) { // Player holding right
-		hero.x += hero.speed * modifier;
-	}
-
-	// Are they touching?
-	if (hero.x <= (monster.x + 32)
-		&& monster.x <= (hero.x + 32)
-		&& hero.y <= (monster.y + 32)
-		&& monster.y <= (hero.y + 32)) {
-		++monstersCaught;
-		reset();
-	}
-};
-
-var init = function() {
-	layer.add(bgImage);
-
-	heroImage.setX(hero.x)
-	heroImage.setY(hero.y)
-	layer.add(heroImage);
-
-	monsterImage.setX(monster.x);
-	monsterImage.setY(monster.y);
-	layer.add(monsterImage);
-
-	layer.add(score);
-	stage.add(layer);
-}
-
-stage.onFrame(function(frame){
-	now = Date.now();
-	delta = now - then;
-	update(delta/1000);
 	
-	if (heroReady) {
+	var handleMouse = function (e) {
+		hero.x = e.clientX;
+		hero.y = e.clientY;
+	}
+	*/
+	
+	// Reset the game status
+	var reset = function () {
+		then = Date.now();
+		hero.x = stage.getWidth() / 2;
+		hero.y = stage.getHeight() / 2;
+	
+		// Throw the monster somewhere on the screen randomly
+		monster.x = 32 + (Math.random() * (stage.getWidth() - 64));
+		monster.y = 32 + (Math.random() * (stage.getHeight() - 64));
+		arrowImage.hide();
+	};
+	
+	var gameOverStatus = function() {
+		gameOverText.setX(stage.getWidth() / 3);
+		gameOverText.setY(stage.getHeight() / 2);
+		gameOverText.show();
+		gameOver = true;
+	}
+	
+	// Update game objects
+	var update = function (modifier) {
+		if (!gameOver) {
+			// Player holding up
+			if (38 in keysDown) { 
+				hero.y -= hero.speed * modifier;
+			}
+			// Player holding down
+			if (40 in keysDown) { 
+				hero.y += hero.speed * modifier;
+			}
+			// Player holding left
+			if (37 in keysDown) { 
+				hero.x -= hero.speed * modifier;
+			}
+			// Player holding right
+			if (39 in keysDown) { 
+				hero.x += hero.speed * modifier;
+			}
+			// Player holding right
+			if (17 in keysDown) {
+				if (!arrow.moving) {
+					arrowImage.setX(hero.x + heroImage.getImage().width);
+					arrowImage.setY(hero.y);
+					arrow.moving = true;
+					arrowImage.show();
+				}
+			}
+		
+			if (arrowImage.isVisible()) {
+				arrowImage.setX(arrowImage.getX() + arrow.speed * modifier);
+			}
+			
+			// Monster killed?
+			if (arrowImage.getX() <= (monster.x + 32)
+				&& monster.x <= (arrowImage.getX() + 32)
+				&& hero.y <= (monster.y + 32)
+				&& monster.y <= (arrowImage.getY() + 32)) {
+				++monstersCaught;
+				arrow.moving = false;
+				reset();
+			}
+			
+			if (arrowImage.getX() > stage.getWidth() + 32) {
+				arrowImage.hide();
+				arrow.moving = false;
+			}
+
+			// Are they touching?
+			if (hero.x <= (monster.x + 32)
+				&& monster.x <= (hero.x + 32)
+				&& hero.y <= (monster.y + 32)
+				&& monster.y <= (hero.y + 32)) {
+				lives--;
+				if (lives == 0) {
+					gameOverStatus();
+				}
+				reset();
+			}
+		}
+	};
+	
+	// Initialize game
+	var init = function() {
+		reset();
+		
+		lives = 3;
+		monstersCaught = 0;
+
+		layer.add(bgImage);
+	
 		heroImage.setX(hero.x)
 		heroImage.setY(hero.y)
-	}
-
-	if (monsterReady) {
+		layer.add(heroImage);
+	
 		monsterImage.setX(monster.x);
 		monsterImage.setY(monster.y);
+		layer.add(monsterImage);
+
+		gameOverText.hide();
+		layer.add(arrowImage);
+		layer.add(scoreText);
+		layer.add(livesText);
+		layer.add(gameOverText);
+		stage.add(layer);
 	}
-
-	// Score
-	score.setText("Goblins caught: " + monstersCaught);
-	layer.draw();
-	then = now;
-});
-
-// Let's play this game!
-reset();
-init();
-stage.start();
+	
+	// Main loop
+	stage.onFrame(function(frame){
+		now = Date.now();
+		delta = now - then;
+		update(delta/1000);
+		
+		if (heroReady) {
+			heroImage.setX(hero.x)
+			heroImage.setY(hero.y)
+		}
+	
+		if (monsterReady) {
+			monsterImage.setX(monster.x);
+			monsterImage.setY(monster.y);
+		}
+	
+		// scoreText
+		scoreText.setText("Score: " + monstersCaught);
+		livesText.setText("Lives: " + lives);
+		layer.draw();
+		then = now;
+	});
+	
+	// Let's play this game!
+	init();
+	stage.start();
+}
